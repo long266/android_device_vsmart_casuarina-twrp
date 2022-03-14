46b40e72b00c77ff4143de5940f5387aac7d023f
PainKiller <longdangcap17@gmail.com>
2022-03-14 15:27:42 +0700

    casuarina : Intial TWRP Tree

---
+++ omni_casuarina.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+#
+# Copyright (C) 2022 The Team Win Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := casuarina
+
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/vsmart/casuarina/Image.gz-dtb
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+# qcom standard decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := casuarina
+PRODUCT_NAME := omni_casuarina
+PRODUCT_BRAND := Vsmart
+PRODUCT_MODEL := Vsmart Joy 3
+PRODUCT_MANUFACTURER := Vsmart
+
+## wrappedkey
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    fbe.data.wrappedkey=true
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.secure=1 \
+	ro.adb.secure=0 \
+	ro.product.device=casuarina \
+	ro.build.date.utc=1000000000
+
+BUILD_FINGERPRINT := vsmart/casuarina_open/casuarina:10/QKQ1.200311.002/V430A_OPN_U_B15_211112:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=casuarina \
+    PRODUCT_NAME=casuarina
+
